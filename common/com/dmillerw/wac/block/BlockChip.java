a547ff4b8bba75702b2f1242623dc5e217ee88c8
Dylan Miller <dmillerw@gmail.com>
2013-04-12 20:27:03 -0700

    Made chip drop if no block supporting it

--- common/com/dmillerw/wac/block/BlockChip.java
+++ common/com/dmillerw/wac/block/BlockChip.java
@@ -1,16 +1,18 @@
 package com.dmillerw.wac.block;
 
-import com.dmillerw.wac.WACMain;
-import com.dmillerw.wac.interfaces.IAttachedToSide;
-import com.dmillerw.wac.tileentity.TileEntityChip;
-
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.AxisAlignedBB;
+import net.minecraft.util.Icon;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import net.minecraftforge.common.ForgeDirection;
+
+import com.dmillerw.wac.WACMain;
+import com.dmillerw.wac.interfaces.IAttachedToSide;
+import com.dmillerw.wac.tileentity.TileEntityChip;
 
 public class BlockChip extends BlockContainer {
 
@@ -24,6 +26,40 @@ public class BlockChip extends BlockContainer {
 	}
 	
 	public AxisAlignedBB getCollisionBoundingBoxFromPool(World world, int x, int y, int z) {return null;}
+	
+	public void onNeighborBlockChange(World world, int x, int y, int z, int blockID) {
+		IAttachedToSide attached = (IAttachedToSide) world.getBlockTileEntity(x, y, z);
+		
+		int xOrig = x;
+		int yOrig = y;
+		int zOrig = z;
+		
+		if (attached == null) {
+			this.dropBlockAsItem(world, xOrig, yOrig, zOrig, world.getBlockMetadata(xOrig, yOrig, zOrig), 0);
+			world.setBlock(xOrig, yOrig, zOrig, 0);
+			return;
+		}
+		
+		x += attached.getSideAttached().offsetX;
+		y += attached.getSideAttached().offsetY;
+		z += attached.getSideAttached().offsetZ;
+		
+		if (Block.blocksList[world.getBlockId(x, y, z)] == null) {
+			this.dropBlockAsItem(world, xOrig, yOrig, zOrig, world.getBlockMetadata(xOrig, yOrig, zOrig), 0);
+			world.setBlock(xOrig, yOrig, zOrig, 0);
+			return;
+		}
+		
+		if (!Block.blocksList[world.getBlockId(x, y, z)].isBlockSolidOnSide(world, x, y, z, attached.getSideAttached())) {
+			this.dropBlockAsItem(world, xOrig, yOrig, zOrig, world.getBlockMetadata(xOrig, yOrig, zOrig), 0);
+			world.setBlock(xOrig, yOrig, zOrig, 0);
+			return;
+		}
+	}
+	
+	public Icon getIcon(int side, int meta) {
+		return Block.blockIron.getIcon(side, meta);
+	}
 	
 	public void setBlockBoundsBasedOnState(IBlockAccess world, int x, int y, int z) {
 		IAttachedToSide attached = (IAttachedToSide) world.getBlockTileEntity(x, y, z);
