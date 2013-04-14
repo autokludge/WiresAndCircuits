b97490a8bb82750a8f11340986de54f04017e374
Dylan Miller <dmillerw@gmail.com>
2013-04-13 19:23:02 -0700

    Registered all math gates

--- common/com/dmillerw/wac/gates/GateManager.java
+++ common/com/dmillerw/wac/gates/GateManager.java
@@ -9,7 +9,23 @@ import com.dmillerw.wac.gates.arithmatic.GateAddition;
 import com.dmillerw.wac.gates.arithmatic.GateAndAdd;
 import com.dmillerw.wac.gates.arithmatic.GateAverage;
 import com.dmillerw.wac.gates.arithmatic.GateCeiling;
+import com.dmillerw.wac.gates.arithmatic.GateDelta;
 import com.dmillerw.wac.gates.arithmatic.GateDivide;
+import com.dmillerw.wac.gates.arithmatic.GateExponentialPowers;
+import com.dmillerw.wac.gates.arithmatic.GateFloor;
+import com.dmillerw.wac.gates.arithmatic.GateIdentity;
+import com.dmillerw.wac.gates.arithmatic.GateIncDec;
+import com.dmillerw.wac.gates.arithmatic.GateIncrement;
+import com.dmillerw.wac.gates.arithmatic.GateInverse;
+import com.dmillerw.wac.gates.arithmatic.GateModulo;
+import com.dmillerw.wac.gates.arithmatic.GateMultiply;
+import com.dmillerw.wac.gates.arithmatic.GateNegate;
+import com.dmillerw.wac.gates.arithmatic.GatePi;
+import com.dmillerw.wac.gates.arithmatic.GateRandom;
+import com.dmillerw.wac.gates.arithmatic.GateRound;
+import com.dmillerw.wac.gates.arithmatic.GateSign;
+import com.dmillerw.wac.gates.arithmatic.GateSquareRoot;
+import com.dmillerw.wac.gates.arithmatic.GateSubtract;
 
 public class GateManager {
 
@@ -33,12 +49,37 @@ public class GateManager {
 	}
 	
 	public static void initializeDefaultGates() {
+		//Any commented gates still need to be implemented
+		
+		/* ARITHMATIC */
 		registerGate(0, GateAbsolute.class);
 		registerGate(1, GateAddition.class);
 		registerGate(2, GateAndAdd.class);
 		registerGate(3, GateAverage.class);
 		registerGate(4, GateCeiling.class);
 		registerGate(5, GateDivide.class);
+		registerGate(6, GateDelta.class);
+//		registerGate(7, GateDeltaRec.class);
+		registerGate(8, GateDivide.class);
+//		registerGate(9, GateExp.class);
+		registerGate(10, GateExponentialPowers.class);
+		registerGate(11, GateFloor.class);
+		registerGate(12, GateIdentity.class);
+		registerGate(13, GateIncrement.class);
+		registerGate(14, GateIncDec.class);
+		registerGate(15, GateInverse.class);
+//		registerGate(16, GateLog.class);
+//		registerGate(17, GateLog10.class);
+		registerGate(18, GateModulo.class);
+		registerGate(19, GateMultiply.class);
+		registerGate(20, GateNegate.class);
+		registerGate(21, GatePi.class);
+//		registerGate(22, GatePercent.class);
+		registerGate(23, GateRandom.class);
+		registerGate(24, GateRound.class);
+		registerGate(25, GateSign.class);
+		registerGate(26, GateSquareRoot.class);
+		registerGate(27, GateSubtract.class);
 	}
 	
 }
