b6120662f807dd77f2c4142ac2a77e10aad8b25a
Dylan Miller <dmillerw@gmail.com>
2013-04-15 20:44:07 -0700

    Vewy vewy hawd

--- common/com/dmillerw/wac/client/render/TileEntityGateRenderer.java
+++ common/com/dmillerw/wac/client/render/TileEntityGateRenderer.java
@@ -3,8 +3,6 @@ package com.dmillerw.wac.client.render;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
 import net.minecraft.tileentity.TileEntity;
 
-import org.lwjgl.opengl.GL11;
-
 import com.dmillerw.wac.tileentity.TileEntityGate;
 import com.dmillerw.wac.util.BlockCoord;
 import com.dmillerw.wac.util.GateConnection;
@@ -13,14 +11,7 @@ public class TileEntityGateRenderer extends TileEntitySpecialRenderer {
 	
 	//VERY TEMPORARY
 	public void renderWire(BlockCoord start, BlockCoord end) {
-		GL11.glPushMatrix();
-		GL11.glColor3f(0,0,0);
-		GL11.glTranslated(start.x, start.y, start.z);
-	    GL11.glBegin(GL11.GL_LINES);
-	    GL11.glVertex3f(0, 0, 0);
-	    GL11.glVertex3f(end.x, end.y, end.z);
-	    GL11.glEnd();
-	    GL11.glPopMatrix();
+
 	}
 	
 	@Override
